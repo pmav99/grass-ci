f7c2c52afd4a661ba0a2cac42930a51466ed9b6b
Panos Mavrogiorgos <pmav99@gmail.com>
2019-03-04 20:09:58 +0200

    Make GRASS library importable.

--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -1,4 +1,39 @@
+import gettext
 import os
+
+from os.path import dirname
+
+# _ROOT_DIR points to the root directory of the GRASS installation/distribution
+# Yeap, calling 4 times dirname is not really elegant, but we want to go from:
+#     dist.x86_64-pc-linux-gnu/etc/python/grass/__init__.py
+# to:
+#     dist.x86_64-pc-linux-gnu/
+#
+_ROOT_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
+
+# Setup i18N
+#
+# Calling `gettext.install()` installs `_()` in the builtins namespace and thus it
+# becomes available globally (i.e. in the same process). We need to do this here
+# to ensure that the injection happens before anything else gets imported.
+#
+# For more info please check the following links:
+# - https://docs.python.org/2/library/gettext.html#gettext.install
+# - https://pymotw.com/2//gettext/index.html#application-vs-module-localization
+# - https://www.wefearchange.org/2012/06/the-right-way-to-internationalize-your.html
+#
+_LOCALE_DIR = os.path.join(_ROOT_DIR, "locale")
+
+# XXX not really necessary + it will fail when GRASS is compiled without NLS
+# XXX but it does make debugging easier.
+if not os.path.exists(_LOCALE_DIR):
+    raise ValueError("LOCALE could not be found: %s" % _LOCALE_DIR)
+
+gettext.install('grasslibs', _LOCALE_DIR)
+gettext.install('grassmods', _LOCALE_DIR)
+gettext.install('grasswxpy', _LOCALE_DIR)
+
+
 __all__ = ["script", "temporal"]
 if os.path.exists(os.path.join(os.path.dirname(__file__), "lib", "__init__.py")):
     __all__.append("lib")
